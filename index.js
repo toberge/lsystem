7b92e8db6321eda56e8a82e6ea60082c388efe7a
Tore Bergebakken <toberge@stud.ntnu.no>
2023-03-22 19:14:02 +0100

    Add config UI

--- index.js
+++ index.js
@@ -18,31 +18,31 @@ let config = {
   depth: 4,
 };
 
-const systems = [
-  {
-    name: "bush",
+const systems = {
+  bush: {
+    name: "Bush",
     axiom: "F",
     rules: {
-      F: "FF-[-F+F+F]+[+F-F-FX]",
+      F: "FF-[-F+F+F]+[+F-F-F$]",
     },
     angle: (22.5 / 180) * Math.PI,
     falloff: 1,
     length: 16,
   },
-  {
+  fork: {
     name: "Fork",
     axiom: "F",
     rules: {
       //F: "F[-F-F[-F+F]+F-F]+F+F[-F+F]+F-F",
-      F: "FF[-F[+FX]][+F[-FX]][-F[-FX]]", // add [+F[+FX]] for symmetry
+      F: "FF[-F[+F$]][+F[-F$]][-F[-F$]]", // add [+F[+FX]] for symmetry
     },
     depth: 3,
     angle: Math.PI / 6,
     length: 50,
     falloff: 0.95,
   },
-  {
-    name: "MengerSponge",
+  menger: {
+    name: "Menger Sponge",
     axiom: "F-F-F-F",
     rules: { F: "FF-F-F-F-FF" },
     angle: Math.PI / 2,
@@ -50,27 +50,17 @@ const systems = [
     length: 4,
     depth: 4,
   },
-];
+};
+
+config = { ...config, ...systems["fork"] };
 
 const createSystem = (axiom, rules, depth = 5) => {
   let system = axiom.split("");
-  console.log(axiom, rules);
   for (let i = 0; i < depth; i++) {
     system = system.flatMap((c) => (rules[c] ? rules[c].split("") : c));
-    console.log(system.join(""));
   }
   return system.join("");
 };
-
-config = { ...config, ...systems[1] };
-const system = createSystem(config.axiom, config.rules, config.depth);
-
-// Palette
-// #E8B50E
-// #9C7702
-// #1AA3E8
-// #E85825
-// #9C4121
 
 const draw = (path) => {
   context.strokeStyle = "#9C7702";
@@ -93,7 +83,7 @@ const draw = (path) => {
         params.length *= config.falloff;
         context.globalCompositeOperation = "source-over";
         break;
-      case "X":
+      case "$":
         context.translate(0, params.length / 2);
         context.beginPath();
         context.arc(
@@ -127,4 +117,91 @@ const draw = (path) => {
   }
 };
 
-draw(system);
+const refresh = () => {
+  requestAnimationFrame(() =>
+    draw(createSystem(config.axiom, config.rules, config.depth))
+  );
+};
+
+refresh();
+
+const presetSelect = document.getElementById("preset");
+Object.keys(systems).forEach((id) => {
+  const option = document.createElement("option");
+  option.value = id;
+  option.innerHTML = systems[id].name;
+  presetSelect.appendChild(option);
+});
+presetSelect.addEventListener("change", (e) => {
+  const system = systems[e.target.value] || systems.fork;
+  config = { ...config, ...system };
+  refresh();
+});
+
+const hookUpInput = (id, transform = (x) => x) => {
+  const input = document.getElementById(id);
+  input.value = config[id];
+  input.addEventListener("change", (e) => {
+    config[id] = transform(e.target.value);
+    refresh();
+  });
+  presetSelect.addEventListener("change", () => {
+    input.value = config[id];
+  });
+};
+hookUpInput("length", parseFloat);
+hookUpInput("angle", parseFloat);
+hookUpInput("depth", parseInt);
+
+const axiomInput = document.getElementById("axiom");
+axiomInput.value = config.axiom;
+presetSelect.addEventListener("change", () => {
+  axiomInput.value = config.axiom;
+});
+axiomInput.addEventListener("change", (e) => {
+  config.axiom = e.target.value;
+  refresh();
+});
+axiomInput.addEventListener("keyup", (e) => {
+  if (e.key == "Enter" || e.keyCode == 13) {
+    config.axiom = axiomInput.value;
+    refresh();
+  }
+});
+
+const rulesInput = document.getElementById("rules");
+const setRules = (input) => {
+  let result;
+  try {
+    result = JSON.parse(input);
+    if (
+      typeof result !== "object" ||
+      Object.values(result).some((rule) => typeof rule !== "string")
+    ) {
+      result = new Error('Rules should be of the form { "X": "Y" }');
+    }
+  } catch (e) {
+    result = new Error("That ain't proper JSON");
+  }
+
+  if ("message" in result) {
+    console.error(result);
+    rulesInput.style.setProperty("border-color", "#E85825");
+  } else {
+    config.rules = result;
+    rulesInput.style.setProperty("border-color", "transparent");
+    refresh();
+  }
+};
+rulesInput.value = JSON.stringify(config.rules, null, 2);
+presetSelect.addEventListener("change", () => {
+  rulesInput.value = JSON.stringify(config.rules, null, 2);
+});
+rulesInput.addEventListener("change", (e) => {
+  setRules(e.target.value);
+});
+rulesInput.addEventListener("keyup", (e) => {
+  if ((e.key == "Enter" || e.keyCode == 13) && (e.ctrlKey || e.altKey)) {
+    setRules(rulesInput.value);
+  }
+});
